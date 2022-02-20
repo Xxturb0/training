93757b97f0f6771623f36e8bfbc9825c2b17200f
msyaifullah10 <ipul.brayen10@gmail.com>
2022-02-20 18:40:47 +0700

    update

---
+++ Index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=divice-width", initial-sca]>
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="tampilan.css">
+</head>
+<body>
+	<div class="badan-utama">
+        
+        <header>
+            <h2>Toko Sepeda Jaya</h2>
+            <p>Toko Sepeda Terpercaya</p>
+        </header>
+ 
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="http://localhost/ta-prpl2021-c-1900018160-aplikasi-penjualan-sepeda-jaya/index.php">Home</a></li>
+                <li><a href="">Jenis Sepeda</a></li>
+                <li><a href="">Pesan</a></li>
+            </ul>
+        </nav>
+ 
+        <div class="banner">
+            <img src="Motor.jpg" alt="Toko Sepeda Jasa">
+        </div>
+ 
+        <div class="menu-kiri">
+            <div class="kotak">
+                <h3>Alamat</h3>
+                <p>
+                    Jl.Bawal No.3 Yogyakarta
+                </p>
+            </div>
+            <div class="kotak">
+                <h3>Pesan</h3>
+                <p>Isi Form Disini Untuk Beli</p>
+                <a class="tombol tombol-pesan" href="">Pesan</a>
+            </div>
+
+        </div>
+        
+ 
+        <div class="menu-tengah">
+            <div class="kotak">
+                <h3>Toko Sepeda Jaya</h3>
+                <span class="tanggal-posting">
+                    <!-- Diposting pada 15.11 WITA, 13 Juli 2020 -->
+                </span>
+ 
+                <img src="Motor.jpg" alt="Sepeda">
+ 
+                <p>
+                </p>
+ 
+                <a class="tombol tombol-lengkap" href="#">Selengkapnya</a>
+            </div>
+        </div>
+ 
+        <div class="menu-kanan">
+            <div class="kotak">
+                <h3>Jadwal Buka</h3>
+                <img src="Motor.jpg" alt="Sepeda">
+ 
+                <h4 align="center">Jadwal Buka</h4>
+                <center>
+                    <p>Senin-Minggu 08.00 WIB - 17.30 WIB</p>
+                </center>
+            </div>
+ 
+ 
+            <div class="kotak">
+                <h3>Berita Terbaru</h3>
+ 
+                <nav class="menu-artikel">
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+ 
+        <footer>
+            @copyright 2020 || M.S
+        </footer>
+ 
+    </div>
+ 
+
+</body>
+</html>
